29c21e5c81c3d014c09a247e1e4dc885f195ee32
Jan Milfait <honza.keno@seznam.cz>
2023-12-21 14:11:29 +0100

    Fix colors

--- src/ChevronChart.js
+++ src/ChevronChart.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import Chevron from "./Chevron";
 
-const ChevronChart = ({checkBoxes}) => {
-    // Filter out unchecked checkboxes to keep colors consistent
-    const onlyChecked = checkBoxes.filter((item) => item)
-    return (
-        <div className="chevronChart">
-            {onlyChecked.map((checked, index) => {
-                const color = `rgba(0, 0, 0, ${1 - index / onlyChecked.length})`
-                return checked ? <Chevron key={`chevron-${index}`} color={color} /> : null
-            })}
-        </div>
-    );
-}
+const ChevronChart = ({checkBoxes}) =>
+    <div className="chevronChart">
+        {checkBoxes.map((checked, index) => {
+            const color = `rgba(0, 0, 0, ${1 - index / checkBoxes.length})`
+            return checked ? <Chevron key={`chevron-${index}`} color={color} /> : null
+        })}
+    </div>
 
 export default ChevronChart;
